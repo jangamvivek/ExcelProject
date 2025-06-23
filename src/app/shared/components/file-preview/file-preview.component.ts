3161b6b7e25ee97ac3c0df0831f4d6677ecd87e5
vivek jangam <vivekjangam19@gmail.com>
2025-06-23 17:30:07 +0530

    ppt done

---
+++ src/app/shared/components/file-preview/file-preview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-preview',
+  standalone: false,
+  templateUrl: './file-preview.component.html',
+  styleUrl: './file-preview.component.css'
+})
+export class FilePreviewComponent {
+
+}
